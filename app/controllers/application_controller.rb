a39e1751601f70d9a8483ad7667e0e4e4ade6348
Helen Hood <helen.c.hood@gmail.com>
2014-12-23 15:33:43 -0500

    Handle errors where record does not exist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def record_not_found
+    render json: { message: "Resource not found" },
+      status: 404
+  end
 end
